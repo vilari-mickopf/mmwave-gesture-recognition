261ac7986d7ecc5544c7ee247ceb1a5be7ed74b1
Filip Markovic <f12markovic@gmail.com>
2023-07-08 00:13:02 +0200

    Separate pad and polar preprocessors

--- mmwave/data/preprocessor.py
+++ mmwave/data/preprocessor.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python
 
+from abc import ABC, abstractmethod
+
 import numpy as np
 import tensorflow as tf
 
 
-class PolarPreprocessor:
-    def __init__(self, formats, num_of_objs=65, num_of_frames=50):
-        self.formats = formats
+class Preprocessor(ABC):
+    @abstractmethod
+    def process(self, sample):
+        pass
+
+
+class ZeroPadd(Preprocessor):
+    def __init__(self, num_of_objs=65, num_of_frames=50):
         self.num_of_objs = num_of_objs
         self.num_of_frames = num_of_frames
-
         self.empty = {'rho': 0., 'theta': 0., 'doppler': 0., 'peak': 0.}
-
-    def process_obj(self, obj):
-        x, y = obj['x'], obj['y']
-        rho = np.sqrt(x**2 + y**2)
-        theta = np.arctan2(y, x)
-        if rho <= 1:
-            return {
-                'rho': rho, # already in range 0-1
-                'theta': theta/np.pi,
-                'doppler': obj['doppler']/self.formats.max_velocity,
-                'peak': obj['peak']/(10*np.log10(1 + 2**16))
-            }
-
-        return None
 
     def zero_pad_objs(self, data):
         return tf.keras.preprocessing.sequence.pad_sequences(
@@ -39,6 +31,31 @@ class PolarPreprocessor:
         return np.concatenate((data, padding), axis=0)
 
     def process(self, sample):
+        data = self.zero_pad_objs([frame[:self.num_of_objs] for frame in sample])
+        data = self.zero_pad_frames(data[:self.num_of_frames])
+        return data
+
+
+class Polar(Preprocessor):
+    def __init__(self, formats):
+        self.formats = formats
+        self.empty = {'rho': 0., 'theta': 0., 'doppler': 0., 'peak': 0.}
+
+    def process_obj(self, obj):
+        x, y = obj['x'], obj['y']
+        rho = np.sqrt(x**2 + y**2)
+        theta = np.arctan2(x, y)
+        if rho <= 1:
+            return {
+                'rho': rho, # already in range 0-1
+                'theta': theta/(np.pi/2),
+                'doppler': obj['doppler']/self.formats.max_velocity,
+                'peak': obj['peak']/(10*np.log10(1 + 2**16))
+            }
+
+        return None
+
+    def process(self, sample):
         data = []
         for frame in sample:
             objs = [list(processed_obj.values()) for obj in (frame or [])
@@ -47,15 +64,6 @@ class PolarPreprocessor:
             if not objs:
                 objs.append(list(self.empty.values()))
 
-            if len(objs) > self.num_of_objs:
-                objs = objs[:self.num_of_objs]
-
             data.append(objs)
 
-        data = self.zero_pad_objs(data)
-        if len(data) > self.num_of_frames:
-            # print('Warning?')
-            data = data[:self.num_of_frames]
-
-        data = self.zero_pad_frames(data)
         return data
